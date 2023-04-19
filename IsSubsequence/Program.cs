c411d1864a5ba88ec762e639654b5c7628b77580
Chris <krzysiek.palonek@gmail.com>
2023-04-19 18:06:43 +0200

    19.04

---
+++ IsSubsequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+Solution test = new Solution();
+Console.WriteLine( test.IsSubsequence( "", "ahbgdc" ) );
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0) return true;
+        StringBuilder first = new StringBuilder();
+        for (int i = 0, j = 0; i < t.Length; i++)
+        {
+            if (t[i] == s[j])
+            {
+                first.Append( t[i] );
+                j++;
+            }
+        }
+
+        if (first.ToString() == s)
+            return true;
+
+        return false;
+    }
+}
--- Isomorphic Strings/Program.cs
+++ Isomorphic Strings/Program.cs
@@ -1,42 +1,59 @@
 ﻿Solution solution = new Solution();
 
-Console.WriteLine( solution.IsIsomorphic( "badc", "baba" ) );
+Console.WriteLine( solution.IsIsomorphic( "paper", "title" ) );
 
 public class Solution
 {
     public bool IsIsomorphic(string s, string t)
     {
-        if (s == t)
+        if (s.Length != t.Length)
         {
-            return true;
+            return false;
         }
 
-        List<int> indexElements = new List<int>();
-        for (int i = 0; i < s.Length; i++)
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.Length;
+
+        for (int i = 0; i < n; ++i)
         {
-            char letter = s[i];
-            for (int j = i + 1; j < s.Length; j++)
-            {
-                if (indexElements.Count - 1 >= s.Length) break;
-                if (s[j] == letter)
-                {
-                    if (!indexElements.Contains( i )) indexElements.Add( i );
-
-                    if (!indexElements.Contains( j )) indexElements.Add( j );
-                }
-            }
-        }
-
-        if (indexElements.Count == 0) return false;
-
-        for (int i = 0; i < indexElements.Count; i++)
-        {
-            if (!(t[indexElements[i]] == t[indexElements[i + 1]]))
+            if (m1[s[i]] != m2[t[i]])
             {
                 return false;
             }
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
-
         return true;
+
+        //dictonary.
+
+        //List<int> indexElements = new List<int>();
+        //for (int i = 0; i < s.Length; i++)
+        //{
+        //    char letter = s[i];
+        //    for (int j = i + 1; j < s.Length; j++)
+        //    {
+        //        if (indexElements.Count - 1 >= s.Length) break;
+        //        if (s[j] == letter)
+        //        {
+        //            if (!indexElements.Contains( i )) indexElements.Add( i );
+
+        //            if (!indexElements.Contains( j )) indexElements.Add( j );
+        //        }
+        //    }
+        //}
+
+        //if (indexElements.Count == 0) return false;
+
+        //for (int i = 0; i < indexElements.Count; i++)
+        //{
+        //    if (!(t[indexElements[i]] == t[indexElements[i + 1]]))
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //return true;
     }
 }
