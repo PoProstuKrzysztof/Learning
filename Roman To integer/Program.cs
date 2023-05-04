a542b61966e731bd7f84fd2485f288cdb2820cec
Chris <krzysiek.palonek@gmail.com>
2023-05-05 01:20:41 +0200

    05.05

--- Roman To integer/Program.cs
+++ Roman To integer/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine( "Hello, World!" );
+﻿using System.Collections;
+
+Console.WriteLine( Solution.RomanToInt( "MCMXCIV" ) );
+
+public static class Solution
+{
+    public static int RomanToInt(string s)
+    {
+        Dictionary<string, int> romans = new Dictionary<string, int>();
+        romans.Add( "I", 1 );
+        romans.Add( "V", 5 );
+        romans.Add( "X", 10 );
+        romans.Add( "L", 50 );
+        romans.Add( "C", 100 );
+        romans.Add( "D", 500 );
+        romans.Add( "M", 1000 );
+        romans.Add( "IV", 4 );
+        romans.Add( "IX", 9 );
+        romans.Add( "XL", 40 );
+        romans.Add( "XC", 90 );
+        romans.Add( "CD", 400 );
+        romans.Add( "CM", 900 );
+
+        int result = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (romans.ContainsKey( s[i].ToString() ))
+            {
+                int firstDigit = romans.GetValueOrDefault( s[i].ToString() );
+                if (i == s.Length - 1)
+                {
+                    result += firstDigit;
+                    break;
+                }
+                string val = s[i].ToString() + s[i + 1].ToString();
+                if (romans.ContainsKey( val ))
+                {
+                    result += romans.GetValueOrDefault( val );
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    result += firstDigit;
+                }
+            }
+        }
+        // sprawia że wygrywasz memory w 99%
+        GC.Collect();
+        return result;
+    }
+}
--- Two Sum/Program.cs
+++ Two Sum/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine( "Hello, World!" );
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int subtract = new int();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            subtract = target - nums[i];
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[j] - subtract == 0)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { };
+    }
+}
