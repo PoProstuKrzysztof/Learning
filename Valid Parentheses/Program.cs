abd3fd96b3f693a89ed5c01ca0c0f1cd0c724fa0
Chris <krzysiek.palonek@gmail.com>
2023-05-20 23:00:05 +0200

    20.05

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine( "Should be false: " + Solution.IsValid( "(])" ) );
+Console.WriteLine( "Should be true: " + Solution.IsValid( "()" ) );
+Console.WriteLine( "Should be true: " + Solution.IsValid( "()[]{}" ) );
+Console.WriteLine( "Should be true: " + Solution.IsValid( "{[]}" ) );
+
+public static class Solution
+{
+    public static bool IsValid(string s)
+    {
+        Stack<int> stack = new Stack<int>();
+        foreach (var c in s)
+        {
+            if (c == '[' || c == '{' || c == '(')
+            {
+                stack.Push( c );
+            }
+            else
+            {
+                if (stack.Count == 0) return false;
+                else if (c == '}' && stack.Peek() == '{') { stack.Pop(); }
+                else if (c == ']' && stack.Peek() == '[') { stack.Pop(); }
+                else if (c == ')' && stack.Peek() == '(') { stack.Pop(); }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        GC.Collect();
+        return stack.Count == 0 ? true : false;
+    }
+}
