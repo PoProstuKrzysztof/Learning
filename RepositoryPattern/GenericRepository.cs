c83b9070763adb6f09e85ec8570d05ef01da6915
Chris <krzysiek.palonek@gmail.com>
2023-06-10 12:58:13 +0200

    10.06

---
+++ RepositoryPattern/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly DbContext _context;
+
+    public GenericRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public void Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public async Task<T>? FindAsync(Guid? id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+}
---
+++ RepositoryPattern/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T>? FindAsync(Guid? id);
+
+    void Create(T entity);
+}
